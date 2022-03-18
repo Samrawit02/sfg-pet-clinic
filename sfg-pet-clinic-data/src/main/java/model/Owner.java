8c077de33d5c594e567692b9328ccf46a8bfbfdc
Samrit <hairaer02@gmail.com>
2022-03-18 12:15:34 +0300

    closing tutorial 49

---
+++ sfg-pet-clinic-data/src/main/java/model/Owner.java
@@ -0,0 +1,22 @@
+package model;
+
+import java.util.HashSet;
+import java.util.Set;
+
+/**
+ * Created by jt on 7/13/18.
+ */
+
+public class Owner extends Person {
+
+
+    private String address;
+    private String city;
+    private String telephone;
+
+    private Set<Pet> pets = new HashSet<>();
+
+    }
+
+
+
